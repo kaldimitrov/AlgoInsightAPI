0d3cf6f8d37850306c1499e71b895739c7771c50
Toxic <igntoxicshadow@gmail.com>
2023-11-14 21:55:37 +0200

    Added memory and cpu usage tracking and improved code and security

--- src/docker/docker.service.ts
+++ src/docker/docker.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, Logger } from '@nestjs/common';
-
 import * as Docker from 'dockerode';
 import * as tar from 'tar-stream';
 import { Container } from './models/container.model';
@@ -24,6 +23,11 @@ export class DockerService {
         Tty: true,
         name: 'NodeJs',
         WorkingDir: '/app',
+        NetworkDisabled: true,
+        HostConfig: {
+          AutoRemove: true,
+          NetworkMode: 'none',
+        },
       });
 
       await container.start();
@@ -38,15 +42,27 @@ export class DockerService {
       pack.entry({ name: containerSettings.fileName }, fileContent);
       pack.finalize();
       await container.putArchive(pack, { path: '/app' });
+      const statsStream = await container.stats({ stream: true });
 
+      const statsData = [];
+      statsStream.on('data', (data) => {
+        const processedData = this.processContainerStats(JSON.parse(data.toString('utf-8').replace(/\n/g, '')));
+
+        if (processedData) {
+          statsData.push(processedData);
+        }
+      });
+
+      const output = [];
       let time: number;
-      const output = await Promise.all(
-        containerSettings.execution.map((execStep) => this.execStep(container, execStep)),
-      );
+      for (const execStep of containerSettings.execution) {
+        output.push(await this.execStep(container, execStep));
+      }
 
-      return { time, output };
+      container.wait();
+      return { time, output, statsData };
     } catch (error) {
-      console.error('Error:', error);
+      this.logger.error('Error running docker', error);
       throw error;
     } finally {
       if (!container) {
@@ -55,7 +71,7 @@ export class DockerService {
 
       container.remove({ force: true }, (error) => {
         if (error) {
-          console.error('Error while removing container:', error);
+          this.logger.error('Error while removing container:', error);
         }
       });
     }
@@ -83,5 +99,25 @@ export class DockerService {
     if (execStep.log) {
       return stepOutput;
     }
+  }
+
+  processContainerStats(stats: Docker.ContainerStats) {
+    if (!stats) {
+      return null;
+    }
+
+    const memoryUsage = Math.round(stats.memory_stats.usage / (1024 * 1024));
+
+    const previousCPU = stats.precpu_stats.cpu_usage.total_usage;
+    const previousSystem = stats.precpu_stats.system_cpu_usage;
+    const cpuDelta = stats.cpu_stats.cpu_usage.total_usage - previousCPU;
+    const systemDelta = stats.cpu_stats.system_cpu_usage - previousSystem;
+
+    if (systemDelta <= 0 || cpuDelta <= 0) {
+      return null;
+    }
+    const cpuPercent = (cpuDelta / systemDelta) * stats.cpu_stats.online_cpus * 100;
+
+    return { memoryUsage, cpuPercent };
   }
 }
