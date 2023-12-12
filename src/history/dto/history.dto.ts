2c5baadc2c86bbbc37972b53b259c33b5750bc32
Toxic <igntoxicshadow@gmail.com>
2023-12-12 22:37:09 +0200

    Started history query layout

--- src/history/dto/history.dto.ts
+++ src/history/dto/history.dto.ts
@@ -1,4 +1,33 @@
+import { IsEnum, IsInt, IsNumber, IsOptional, IsString, Max, Min, isPositive } from 'class-validator';
 import { Languages } from 'src/docker/enums/languages';
+import { MAX_PAGE_SIZE } from '../constants';
+
+export class GetHistoryDto {
+  @IsNumber()
+  @IsOptional()
+  min_execution_time?: number;
+
+  @IsNumber()
+  @IsOptional()
+  max_execution_time?: number;
+
+  @IsString()
+  @IsOptional()
+  date_start?: string;
+
+  @IsString()
+  @IsOptional()
+  date_end?: string;
+
+  @IsInt()
+  @Min(0, { message: 'Value must be a positive integer' })
+  page: number;
+
+  @IsInt()
+  @Max(MAX_PAGE_SIZE)
+  @Min(0, { message: 'Value must be a positive integer' })
+  pageSize: number;
+}
 
 export interface CreateHistoryDto {
   language: Languages;
---
+++ src/history/enums/orderOptions.ts
@@ -0,0 +1,11 @@
+export enum OrderTypes {
+  DESC = 'Desc',
+  ASC = 'Asc',
+}
+
+export enum OrderOptions {
+  CREATED_AT = 'created_at',
+  EXECUTION_TIME = 'execution_time',
+  MAX_MEMORY = 'max_memory',
+  MAX_CPU = 'max_cpu',
+}
